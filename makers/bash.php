0427cb374a00443674bd82a9be2ffc1b0149401a
Junling Ma <junlingm@gmail.com>
2015-12-10 20:41:02 -0800

    implemented the first maker: bash. It runs the recipe code as a bash script.

---
+++ makers/bash.php
@@ -0,0 +1,36 @@
+<?php
+
+class Projects_Maker_Bash extends Projects_Maker
+{
+    protected $bash = '';
+
+    public function __construct() {
+        $this->bash = find_executable('bash');
+    }
+
+    public function name() { return "bash"; }
+
+    public function can_handle($file) {
+        return $this->bash && $file->code() != '';
+    }
+
+    public function make($file) {
+        $code = $file->code();
+
+        $line = strtok($code, "\n");
+        if ($line && substr($line, 0, 2) == '#!') {
+            $command = trim(substr($line, 2));
+            $code = trim(substr($code, strlen($line) + 1));
+        } else $command = $this->bash . ' -l';
+
+        $log = $file->log_file();
+        $descs = array(
+            array('pipe', 'r'),
+            array('file', $log, 'w'),
+            array('file', $log, 'w'));
+        $proc = proc_open($command, $descs, $pipes, dirname($log));
+        fwrite($pipes[0], $code);
+        $return = proc_close($proc);
+        return $return === 0;
+    }
+}
