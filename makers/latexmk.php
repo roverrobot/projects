9e54f893de908bced34626bef8c35827eec18aff
Junling Ma <junlingm@gmail.com>
2015-12-14 22:24:30 -0800

    use the new Projects_Maker::dependence()

--- makers/latexmk.php
+++ makers/latexmk.php
@@ -22,14 +22,14 @@ class Projects_Maker_Latexmk extends Projects_Maker
         if (strtolower(substr($file->id(), -4)) != '.pdf') return FALSE;
         if (trim($file->code())) return FALSE;
         $tex = $this->main_latex_file($file->id());
-        $texfile = Projects_file::file($tex);
+        $texfile = self::dependence($tex);
         if ($texfile) return TRUE;
         return FALSE;
     }
 
     public function make($file) {
         $tex = $this->main_latex_file($file->id());
-        $texfile = Projects_file::file($tex);
+        $texfile = self::dependence($tex);
         $path = $texfile->file_path();
         $command = $this->latexmk . " -pdf $path";
         return $this->run($file, $this->bash . ' -l', $command);
@@ -37,8 +37,8 @@ class Projects_Maker_Latexmk extends Projects_Maker
 
     public function auto_dependency($file) {
         $tex = $this->main_latex_file($file->id());
-        $texfile = Projects_file::file($tex);
-        if ($texfile) return array($texfile->id());
+        $texfile = self::dependence($tex);
+        if ($texfile) return array($tex);
         return array(); 
     }
 }
