8bbd029ec9bce35597fe8ecde881cdfb1fc5c282
Junling Ma <junlingm@gmail.com>
2015-12-14 22:10:41 -0800

    fix a stupid mistake: could not count the characters correctly.

--- formatter/image.php
+++ formatter/image.php
@@ -3,11 +3,11 @@
 class Projects_formatter_image extends Projects_formatter {
 	
 	public function can_handle($mimetype) {
-		return ($mimetype && substr($mimetype, 0, 5) == 'image/');
+		return ($mimetype && substr($mimetype, 0, 6) == 'image/');
 	}
 
     public function format($file) {
-    	return '<a href="' . ml($file->id()) . '">' . $file->id() . '</a>';
+    	return '<image src="' . ml($file->id()) . '">' . $file->id() . '</image>';
     }
 
 }
