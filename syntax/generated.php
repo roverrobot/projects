ec53bf77f6bacdb5e3432c292155921be31132a0
Junling Ma <junlingm@gmail.com>
2015-12-14 20:55:30 -0800

    display make errors and make status for all files (not just generated files).

--- syntax/generated.php
+++ syntax/generated.php
@@ -12,44 +12,7 @@ class syntax_plugin_projects_generated extends syntax_projectfile
 {
     protected function type() { return 'generated'; }
 
-    private static function part(&$time, $count, $unit) {
-        $div = floor($time/$count);
-        $val = round($time - $div*$count);
-        $time = $div;
-        if ($val == 0) return '';
-        $result = $val . $unit;
-        if ($val>1) return $result . 's';
-        return $result;
-    }
- 
-    private static function format_time($time) {
-        $sec = self::part($time, 60, ' second');
-        $min = self::part($time, 60, ' minute');
-        $hour = self::part($time, 24, ' hour');
-        $day = self::part($time, 7, ' day');
-        $week = self::part($time, $time+1, ' week');
-        return "$week $day $hour $min $sec";
-    }
-
     protected function content($file) {
-        if ($file->is_making()) {
-            $time = time() - $file->status()->started();
-            $content = '<div id="PROJECTS_progress">The file has been generating for ' . self::format_time($time) . 
-                ': ' . kill_button($file->id(), FALSE) . DOKU_LF;
-            foreach($file->status()->made() as $made) 
-                $content .= '<div class="success">' . html_wikilink($made) . '</div>' . DOKU_LF;
-            $content .= '<div class="notify">' . html_wikilink($file->status()->making()) . '</div>' . DOKU_LF;
-            foreach($file->status()->queue() as $queue) 
-                $content .= '<div class="info">' . html_wikilink($queue) . '</div>' . DOKU_LF;
-            return $content . '</div>' . DOKU_LF; 
-        }
-        if (is_array($file->status())) {
-            $content = '<div>Error in file generation:' . DOKU_LF;
-            foreach($file->status() as $id => $errors)
-                foreach ($errors as $error)
-                    $content .= '<div class="error">' . html_wikilink($id) . ': ' . $error . '</div>' . DOKU_LF;
-            return $content .  '</div>' . DOKU_LF;
-        }
         if ($file->status() === PROJECTS_MODIFIED)
             return '<div>The file is not generated yet: ' . make_button($file->id(), FALSE) . '</div>'; 
         $content = Projects_formatter::xhtml($file);
